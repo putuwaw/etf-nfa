2b7f0ae9498e0b4360b756347c05e9fbe70f5d9d
Putu Widyantara <putuwaw973@gmail.com>
2022-10-05 23:41:53 +0800

    Add form validation

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,1 +1,14 @@
-// scripts.js
+/**
+ * Validate user input
+ * @return true if user input is accepted and false otherwise
+ */
+function validate_form() {
+  let inputValue = document.getElementById("string").value;
+  for (let i = 0; i < inputValue.length; i++) {
+    if (inputValue[i] != "0" && inputValue[i] != "1") {
+      alert("String tidak valid! Hanya diperbolehkan kombinasi 0 atau 1!");
+      return false;
+    }
+  }
+  return true;
+}
